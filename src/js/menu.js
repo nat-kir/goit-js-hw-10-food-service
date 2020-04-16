8338541a23c28602d7c4ecd0222b6d52fe23d27d
Natalia Kiriunina <kiriunina.natalia@gmail.com>
2020-04-16 17:07:36 +0300

    hw-10

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,10 +1,7 @@
 import itemsTemplates from '../templates/menu-items.hbs';
 import menuItems from '../menu.json';
 
-console.log(menuItems);
-
 const markup = itemsTemplates(menuItems);
-
 const menuRef = document.querySelector('.js-menu');
 
 menuRef.insertAdjacentHTML('beforeend', markup);
--- src/js/theme-control.js
+++ src/js/theme-control.js
@@ -1,14 +1,26 @@
 const Theme = {
-    LIGHT: 'light-theme',
-    DARK: 'dark-theme',
-  };
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const currentTheme = localStorage.getItem('Theme');
+const bodyRef = document.querySelector('body');
+const switchBoxRef = document.querySelector('#theme-switch-control');
 
-  const switchRef = document.querySelector("input");
-const bodyRef = document.querySelector("body");
-  console.log(switchRef);
-
-  switchRef.addEventListener('change', function (event) {
-if (switchRef.checked){
-    bodyRef.classList.add(dark-theme);
+if (currentTheme === Theme.DARK) {
+  switchBoxRef.checked = true;
+  bodyRef.classList.add(Theme.DARK);
 }
-  })
+
+switchBoxRef.addEventListener('change', function (event) {
+  if (event.target.checked) {
+    bodyRef.classList.remove(Theme.LIGHT);
+    bodyRef.classList.add(Theme.DARK);
+    switchBoxRef.classList.add('.switch__input:checked')
+    localStorage.setItem('Theme', Theme.DARK);
+  } else {
+    bodyRef.classList.remove(Theme.DARK);
+    bodyRef.classList.add(Theme.LIGHT);
+    switchBoxRef.classList.remove('.switch__input:checked')
+    localStorage.setItem('Theme', Theme.LIGHT);
+  }
+});
